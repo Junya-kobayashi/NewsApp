117af5c0e2aa49d51af5f7e76df050e93f5014bf
小林純也 <atsubeee.ngzk@gmail.com>
2017-09-19 13:29:56 +0900

    newsapp alamofireを適用して見た

---
+++ NewsApp/ArticleListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ArticleListViewController.swift
+//  NewsApp
+//
+//  Created by 小林純也 on 2017/09/19.
+//  Copyright © 2017年 Junya Kobayashi. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+
+class ArticleListViewController: UIViewController {
+    let table = UITableView() //プロパティにtableを追加
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "新着記事"
+        
+        table.frame = view.frame
+        view.addSubview(table)
+    }
+    
+    func getArticles() {
+        Alamofire.request(get, "https://newsapi.org/v1/articles")
+        .responseJSON{ in
+            
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
--- NewsAppTests/NewsAppTests.swift
+++ NewsAppTests/NewsAppTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import NewsApp
+import 
 
 class NewsAppTests: XCTestCase {
     
